63683d94ceb091c44bfe885fa62e860824c1bc89
alexvallau <alexvallau@blyat>
2025-04-04 15:04:14 +0200

    ça fonctionne

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module jsonette
 go 1.23.7
 
 require (
+	github.com/K-Phoen/grabana v0.22.2 // indirect
+	github.com/K-Phoen/sdk v0.12.4 // indirect
 	github.com/google/go-jsonnet v0.20.0 // indirect
-	gopkg.in/yaml.v2 v2.2.7 // indirect
+	github.com/gosimple/slug v1.13.1 // indirect
+	github.com/gosimple/unidecode v1.0.1 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	sigs.k8s.io/yaml v1.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,28 +1,46 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"fmt"
-	"log"
+	"net/http"
+	"os"
+	"strings"
 
-	"github.com/google/go-jsonnet"
+	"github.com/K-Phoen/grabana"
+	"github.com/K-Phoen/grabana/decoder"
 )
 
 func main() {
-	vm := jsonnet.MakeVM()
-
-	snippet := `{
-		person1: {
-		    name: "Alice",
-		    welcome: "Hello " + self.name + "!",
-		},
-		person2: self.person1 { name: "Bob" },
-	}`
-
-	jsonStr, err := vm.EvaluateAnonymousSnippet("example1.jsonnet", snippet)
+	content, err := os.ReadFile("dashboard.yaml")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, "Could not read file: %s\n", err)
+		os.Exit(1)
+	}
+	handler := "405"
+	modifiedContent := []byte(strings.ReplaceAll(string(content), "{{ handler }}", handler))
+
+	dashboard, err := decoder.UnmarshalYAML(bytes.NewBuffer(modifiedContent))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not parse file: %s\n", err)
+		os.Exit(1)
 	}
 
-	fmt.Println(jsonStr)
+	ctx := context.Background()
 
+	ctx = context.WithValue(ctx, "handler", handler)
+	client := grabana.NewClient(&http.Client{}, "http://192.168.141.122:3000", grabana.WithAPIToken("eyJrIjoid1U5NjdGUDNIZ0xCdDN3YlNoa05jWWtuTEZQTk01UmEiLCJuIjoiQVBJIEpTT04iLCJpZCI6MX0="))
+
+	// create the folder holding the dashboard for the service
+	folder, err := client.FindOrCreateFolder(ctx, "Test Folder")
+	if err != nil {
+		fmt.Printf("Could not find or create folder: %s\n", err)
+		os.Exit(1)
+	}
+
+	if _, err := client.UpsertDashboard(ctx, folder, dashboard); err != nil {
+		fmt.Printf("Could not create dashboard: %s\n", err)
+		os.Exit(1)
+	}
 }
